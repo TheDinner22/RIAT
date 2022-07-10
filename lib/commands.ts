c1a1fbc6b235e06fc0eb3e60abef43e3290856cb
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2022-07-10 16:48:18 -0400

    wip

--- lib/commands.ts
+++ lib/commands.ts
@@ -10,13 +10,67 @@
  * 
  */
 
-import { ChildProcess, exec, ExecException } from "child_process";
+import { ChildProcess, execFile, exec, ExecFileException, ExecException } from "child_process";
 
-type processExitHandler = (error: ExecException | null, stdout: string, stderr: string) => void;
+type ExecExitHandler = (error: ExecException | null, stdout: string, stderr: string) => void;
+type ExecFileExitHandler = (error: ExecFileException | null, stdout: string, stderr: string) => void;
+
+
+
+export class NodeApp{
+    fname: string;
+    private args: Array<string>;
+    private nodeApp!: ChildProcess;
+    private startCalled = false;
+    private onExit: ExecFileExitHandler;
+
+
+    constructor(fileName: string, args: string[], onExit: ExecFileExitHandler){
+        this.fname = fileName;
+        this.args = args;
+        this.onExit = onExit;
+    }
+    
+    handleError(error: ExecFileException){
+        console.log("----------------- \x1b[41mERROR\x1b[0m in nodeApp (prolly ur code not mine! LLL) -----------------");
+        console.log("--------------- BEGIN ERROR ---------------\n");
+        console.error(error);
+        console.log("\n---------------- END ERROR ----------------\n");
+    };
+
+    // start
+    start(logWhatApplogs = true){
+        if(this.startCalled){console.log("cannot start twice");return;}
+        this.startCalled = true;
+
+        this.nodeApp = execFile("node", [this.fname, ...this.args], (error, stdout, stderr) => {
+            this.startCalled = false;
+
+            if (error && error.signal != "SIGINT"){this.handleError(error);return;} // bad idea bad idea bad idea!!!
+
+            this.onExit(error, stdout, stderr)
+        });
+
+        if(logWhatApplogs){
+            this.nodeApp.stdout?.on("data", (data) => {
+                console.log(data)
+            });
+        }
+    };
+
+    // stop
+    stop(exitCode: number | NodeJS.Signals = "SIGINT"){
+        if(!this.startCalled){console.log("app is not started! cant stop it");return;}
+        this.startCalled = false;
+        this.nodeApp.kill(exitCode);
+    };
+
+};
+
 
 // BIG FUCKING WARNING!!!! this function is dangerous!!!!!
 // exec is dangerous!!! double triple check whatever you pass into it
-export function command(args: string[], onExit: processExitHandler){
+export function command(args: string[], onExit: ExecExitHandler){
     // make sure there are args
     if(args.length === 0){throw new Error("args was an empty list. Can't do dat");}
 
